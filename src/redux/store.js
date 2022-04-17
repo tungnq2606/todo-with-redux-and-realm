4ba16348abd5f0e81900ad4b414b0171efd8b015
quoctung <tungnq2606@gmail.com>
2022-04-17 21:58:33 +0700

    update

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit';
-import todoReducer from '../TodoList/todoSlice'
+import {configureStore} from '@reduxjs/toolkit';
+import todoReducer from '../TodoList/todoSlice';
 
 const store = configureStore({
-    reducer: {
-      todoList: todoReducer,
-    },
-    middleware: getDefaultMiddleware =>
+  reducer: {
+    todoList: todoReducer,
+  },
+  middleware: getDefaultMiddleware =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
-  });
-  
-  export default store;
-  
+});
+
+export default store;
